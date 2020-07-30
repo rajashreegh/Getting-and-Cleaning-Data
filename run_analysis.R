53ff4aeca617cce3043723a8cf184a4a535ea094
rajashreegh <46857948+rajashreegh@users.noreply.github.com>
2020-07-30 13:32:45 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,15 @@
 library(dplyr)
 
-# read train data
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-# read test data
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-# read data description
 variable_names <- read.table("./UCI HAR Dataset/features.txt")
 
-# read activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 # 1. Merges the training and the test sets to create one data set.
